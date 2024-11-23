514c170c2e3f653d84ad5bc297601b79fb588aea
Deden Alfathimy <dedenhabibi@gmail.com>
2024-11-23 11:06:13 -0500

    Task 3: get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,7 +30,24 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+
+    const author = req.params.author;
+
+    // Filter the books dictionary for any book with the same author
+    let bookswithsameauthor = [];
+
+    for (const [isbn, book] of Object.entries(books)) {
+        if (book.author === author) bookswithsameauthor.push(book);
+    }
+
+    // Response with list of books if there are books with the same author found, otherwise return "No book with the author's name"
+    if (bookswithsameauthor.length > 0) {
+        res.send(bookswithsameauthor);
+    } else {
+        res.status(400).json({ message: "No book authored by " + author + " found."});
+    }
+
+    //return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get all books based on title
